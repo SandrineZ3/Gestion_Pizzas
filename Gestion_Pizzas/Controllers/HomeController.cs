30c2c2f59c65c4a5267f27cd187592f2796aaf9d
SandrineZ3 <79086955+SandrineZ3@users.noreply.github.com>
2022-04-29 15:57:48 +0200

    Début nettoyage des views (Home)

--- Gestion_Pizzas/Controllers/HomeController.cs
+++ Gestion_Pizzas/Controllers/HomeController.cs
@@ -15,14 +15,14 @@ namespace Gestion_Pizzas.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Description fonctionnelle de l'application";
 
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "Mes coordonnées";
 
             return View();
         }
